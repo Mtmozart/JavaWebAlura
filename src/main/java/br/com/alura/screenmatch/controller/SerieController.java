62f3d4085d6da5b992b0ebca6f8f2647d25a1cb3
matheus mozart <mmsnborges@gmail.com>
2024-01-10 12:54:09 -0400

    criando rota top05

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,27 +1,33 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 //entende que é um controller
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService service;
 
     //usado acima do método sobre a requisição que quero obter - seria como route
-    @GetMapping("/series")
+    @GetMapping("")
 
-             public List<SerieDTO> obterSeries () {
-            return service.obterTodasAsSeries();
-        }
-
+    public List<SerieDTO> obterSeries() {
+        return service.obterTodasAsSeries();
+    }
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop05Series(){
+        return service.obterTop05Series();
+   }
 
 
 
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,17 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obterTodasAsSeries(){
-     return  repository.findAll()
-                .stream()
+     return  converteDados(repository.findAll());
+
+    }
+    public List<SerieDTO> obterTop05Series() {
+        return converteDados(repository.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
-
 }
